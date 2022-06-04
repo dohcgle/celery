83d8eea943b129ff0967cc1e599e332e55221ab1
Ulugbek <utn1002@gmail.com>
2022-06-04 06:30:04 -0700

    six

--- webapp/src/proj/settings.py
+++ webapp/src/proj/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    ''
     'main'
 ]
 
@@ -123,3 +122,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+CELERY_BROKER_URL = 'redis://redis:6379'
